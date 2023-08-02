82489679c13ad062eda91c6460b2bca3f18914b0
Aaron Bustos <105888522+AJBustos28@users.noreply.github.com>
2023-08-01 19:59:36 -0600

    Add files via upload

---
+++ AW_Proyecto-avance1/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- POR FAVOR EJECUTAR EN LIVE SERVER -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Pastelería</title>
+    <link rel="stylesheet" href="CSS/styles.css">
+</head>
+
+<body>
+
+    <header class="header" >
+
+        <div class="menu container" >
+            <a href="#" class="logo">Logo</a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+            <img src="images/menuBar.png" class="menu-logo" alt="">
+            <!-- <img src="images/logoKS.png" class="menu-logo" alt=""> -->
+            </label>
+            <nav class="menuBar">
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="postres.php">Postres</a></li>
+                    <li><a href="paquetes.php">Paquetes</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+
+
+        <div class="menuP-content container">
+            <h1>KABY SWEETS</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque architecto veritatis nisi voluptatibus
+                molestias omnis quo sequi aperiam quibusdam odit facilis quis iusto, ducimus aut corrupti incidunt
+                perspiciatis nulla! Provident?
+            </p>
+            <a href="/postres.html" class="boton-1">Ordena aquí</a>
+        </div>
+    </header>
+
+
+
+    <section class="cakes">
+        <!-- <img class="cakes-img" src="images/quequeChocolate.png" alt=""> -->
+        <img class="cakes-img" src="images/sprinkles.png" alt="">
+        <div class="cakes-contenido container">
+            <h2>Tipos de Pasteles disponibles</h2>
+            <p class="texto">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit eos nostrum officia molestias, numquam culpa tenetur
+                dicta consequuntur sint reprehenderit accusamus beatae deserunt quo, facere voluptatem aliquid. Iste, sunt itaque.
+            </p>
+
+            <div class="cakes-grupo">
+
+                <div class="cakes">
+                    <!-- <img src="images/chocoberry.png" alt=""> -->
+                    <img src="images/cupCake.png" alt="">
+                    <h3>Cake lorem</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit eos nostrum officia molestias, numquam culpa tenetur
+                        dicta consequuntur sint reprehenderit accusamus beatae deserunt quo, facere voluptatem aliquid. Iste, sunt itaque.
+                    </p>
+                </div>
+                <div class="cakes">
+                    <!-- <img src="images/redVelvet.png" alt=""> -->
+                    <img src="images/chocoCake.png" alt="">
+                    <h3>Cake lorem</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit eos nostrum officia molestias, numquam culpa tenetur
+                        dicta consequuntur sint reprehenderit accusamus beatae deserunt quo, facere voluptatem aliquid. Iste, sunt itaque.
+                    </p>
+                </div>
+                <div class="cakes">
+                    <!-- <img src="images/tortaChilena.png" alt=""> -->
+                    <img src="images/fruitCake.png" alt="">
+                    <h3>Cake lorem</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit eos nostrum officia molestias, numquam culpa tenetur
+                        dicta consequuntur sint reprehenderit accusamus beatae deserunt quo, facere voluptatem aliquid. Iste, sunt itaque.
+                    </p>
+                </div>
+            </div>
+            <a href="/postres.html" class="boton-1">Ver más</a>
+        </div>
+    </section>
+
+    <main class="servicioP">
+        <div class="servicioP-contenido container">
+            <h2>Servicios Temáticos</h2>
+            <div class="servicioP-grupo">
+                <div class="paquetes">
+                    <img src="images/pack1.png" alt="">
+                    <h3>Comunión</h3>
+                </div>
+                <div class="paquetes">
+                    <img src="images/pack2.png" alt="">
+                    <h3>Cumpleaños</h3>
+                </div>
+                <div class="paquetes">
+                    <img src="images/pack3.png" alt="">
+                    <h3>Bodas</h3>
+                </div>
+            </div>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, porro. Placeat dolorem tempora illum vero, aut nisi
+                officia, magni cupiditate, voluptatum atque neque enim ut quia rem consequuntur voluptatibus a?
+            </p>
+            <a href="/paquetes.html" class="boton-1">info</a>
+        </div>
+    </main>
+
+
+
+
+
+</body>
+
+</html>
---
+++ AW_Proyecto-avance1/postres.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Pastelería - Postres</title>
+    <link rel="stylesheet" href="CSS/styles.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="menu container" >
+            <a href="#" class="logo">Logo</a>
+            <input type="checkbox" id="menu" />
+            <label for="menu">
+            <img src="images/menuBar.png" class="menu-logo" alt="">
+            <!-- <img src="images/logoKS.png" class="menu-logo" alt=""> -->
+            </label>
+            <nav class="menuBar">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="postres.php">Postres</a></li>
+                    <li><a href="paquetes.php">Paquetes</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <!-- Código del encabezado -->
+       <!-- <div class="special-postre">
+            <img src="images/tortaChilenaEspecial.png" alt="Torta Chilena Especial">
+            <div class="postre-info">
+                <h3>Torta Chilena Especial</h3>
+                <p>
+                    Nuestra especialidad de la casa, la "Torta Chilena Especial", es una exquisita combinación de capas de hojarasca crujiente y manjar suave y dulce. Cada bocado es una explosión de sabores que te transportará a los auténticos sabores de Chile.
+                </p>
+                <button class="boton-carrito">Añadir al carrito</button>
+            </div>
+        </div>
+    -->
+    <div class="header">
+        <div class="header-content">
+            <div class="header-column">
+                <h2 class="header-title">Nuestra Historia</h2>
+                <p class="header-text">En nuestra pastelería, cada creación es el resultado de años de tradición y pasión. Nos enorgullece poner nuestro corazón en cada postre que preparamos.</p>
+            </div>
+            <div class="header-column">
+                <h2 class="header-title">Trabajo y Amor</h2>
+                <p class="header-text">Cada postre que sale de nuestra cocina es el resultado de un trabajo arduo y dedicado. Ponemos amor en cada detalle para ofrecerte la mejor experiencia dulce.</p>
+            </div>
+            <div class="header-column">
+                <h2 class="header-title">Ingredientes de Calidad</h2>
+                <p class="header-text">Utilizamos solo los mejores ingredientes en nuestras creaciones. Seleccionamos cuidadosamente cada elemento para asegurarnos de brindarte la más alta calidad en cada bocado.</p>
+            </div>
+        </div>
+    </div>
+    
+        
+        
+    </header>
+
+    <section class="cakes">
+        <!-- Contenido de la sección de postres -->
+        
+        
+            <div class="cakes-contenido container">
+                <h2>Deliciosos Postres</h2>
+                <p class="texto">
+                    Disfruta de nuestra selección de exquisitos postres que te deleitarán con cada bocado. Utilizamos los mejores ingredientes para garantizar una experiencia de sabor única y satisfactoria.
+                </p>
+        
+                <div class="cakes-grupoP">
+                    <div class="cakes">
+                        <img src="images/cupCake.png" alt="Cupcake">
+                        <h3>Cupcake Clásico</h3>
+                        <p>
+                            Nuestro cupcake clásico es perfecto para cualquier ocasión. Su suave textura y delicioso glaseado te transportarán a un mundo de dulzura.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                    </div>
+                    <div class="cakes">
+                        <img src="images/chocoCake.png" alt="Pastel de chocolate">
+                        <h3>Pastel de Chocolate</h3>
+                        <p>
+                            Sumérgete en el placer del chocolate con nuestro pastel de chocolate. Cada rebanada es una explosión de sabor y suavidad que te hará querer más.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                    </div>
+                    <div class="cakes">
+                        <img src="images/fruitCake.png" alt="Pastel de frutas">
+                        <h3>Pastel de Frutas</h3>
+                        <p>
+                            Nuestro pastel de frutas es una combinación perfecta de frescura y dulzura. Cada bocado te hará descubrir una explosión de sabores naturales.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                    </div>
+                    <div class="cakes">
+                        <img src="images/cheesecake.png" alt="Cheesecake">
+                        <h3>Cheesecake de Fresa</h3>
+                        <p>
+                            Nuestro cheesecake de fresa es una delicia irresistible. Con su base crujiente y su relleno cremoso, te hará perder la noción del tiempo.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                    </div>
+                    <div class="cakes">
+                        <img src="images/donut.png" alt="Donut">
+                        <h3>Donut Glaseado</h3>
+                        <p>
+                            Nuestros donuts glaseados son suaves y esponjosos, con un glaseado dulce que los hace irresistibles. El complemento perfecto para tu café de la mañana.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                    </div>
+                    <div class="cakes">
+                        <img src="images/tiramisu.png" alt="Tiramisú">
+                        <h3>Tiramisú Italiano</h3>
+                        <p>
+                            El tiramisú italiano es un postre clásico que combina capas de bizcocho empapado en café, crema de mascarpone y cacao en polvo. Una delicia indulgente que te transportará a Italia.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                    </div>
+
+                    <div class="cakes">
+                        <img src="images/pie.png" alt="Pie de Manzana">
+                        <h3>Pie de Manzana</h3>
+                        <p>
+                            Nuestro pie de manzana casero es un clásico reconfortante. Disfruta de la combinación de manzanas frescas y una deliciosa masa crujiente, todo coronado con un toque de canela.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                        </div>
+                        <div class="cakes">
+                        <img src="images/pannaCotta.png" alt="Panna Cotta">
+                        <h3>Panna Cotta de Vainilla</h3>
+                        <p>
+                        Nuestra panna cotta de vainilla es una delicada y cremosa preparación italiana. Su suavidad y el sabor sutil de la vainilla te cautivarán desde el primer bocado.
+                        </p>
+                        <button class="boton-carrito">Añadir al carrito</button>
+                        </div>
+                        
+                </div>
+
+        
+                <p class="mensaje-postres">
+                    ¿Estás listo para deleitar tu paladar con estos postres deliciosos? Permítete disfrutar de momentos dulces y sabrosos que te llenarán de felicidad. ¡Permítete el placer de los postres!
+                </p>
+            </div>
+        
+        
+        
+    </section>
+
+    <main class="servicioP">
+        <div class="servicioP-contenido container">
+            <h2>Servicios Temáticos</h2>
+            <div class="servicioP-grupo">
+                <div class="paquetes">
+                    <img src="images/pack1.png" alt="">
+                    <h3>Comunión</h3>
+                </div>
+                <div class="paquetes">
+                    <img src="images/pack2.png" alt="">
+                    <h3>Cumpleaños</h3>
+                </div>
+                <div class="paquetes">
+                    <img src="images/pack3.png" alt="">
+                    <h3>Bodas</h3>
+                </div>
+            </div>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo, porro. Placeat dolorem tempora illum vero, aut nisi
+                officia, magni cupiditate, voluptatum atque neque enim ut quia rem consequuntur voluptatibus a?
+            </p>
+            <a href="/paquetes.html" class="boton-1">info</a>
+        </div>
+    </main>
+
+
+    <footer>
+        <!-- Código del pie de página -->
+    </footer>
+
+</body>
+
+</html>
